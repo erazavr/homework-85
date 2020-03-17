d22a2b784cbf0def96589b19db14b3de25239dbb
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-03-17 10:32:16 +0600

    HW#86 - добавил get запрос

--- music-backend/app /track_history.js
+++ music-backend/app /track_history.js
@@ -1,28 +1,28 @@
 const express = require('express');
-
+const auth = require('../middleware/auth');
 const Track_History = require('../models /TrackHistory');
-const Users = require('../models /User');
+const Track = require('../models /Track');
+const Album = require('../models /Album');
+const Artist = require('../models /Artist');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
+    const history = await Track_History.find({user: req.user._id}).sort({datetime: -1});
+    res.send(history)
+});
+router.post('/' ,auth,async (req, res) => {
     const trackHistory = new Track_History(req.body);
-    const authorizationHeader = req.get('Authorization');
-    if (!authorizationHeader) {
-        return res.status(401).send({error: "No authorization header"})
-    }
-    const [type, token] = authorizationHeader.split(' ');
-    if (type !== 'Token' || !token) {
-        return res.status(401).send({error: "Authorizations type wrong or token not present"})
-    }
-    const user = await Users.findOne({token});
-    if (!trackHistory.user) {
-        trackHistory.user = user._id
-    }
-    if (!user) {
-        return res.status(401).send({error: "No user found with this token. Token incorrect"})
-    }
     try {
+        const track = await Track.find({_id: req.body.track});
+        const album = await Album.find({_id: track[0].album});
+        const artist = await Artist.find({_id: album[0].artist});
+        if (!trackHistory.user) {
+            trackHistory.user = req.user._id
+        }
+        trackHistory.artist = artist[0].name;
+        trackHistory.trackName = track[0].name;
+        trackHistory.date();
         await trackHistory.save();
         return res.send(trackHistory)
     } catch (error) {
