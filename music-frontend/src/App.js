6bbd943e4b030979aa09997072ef6ff39c272273
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-03-17 10:43:50 +0600

    HW#86 - добавил header

--- music-frontend/src/App.js
+++ music-frontend/src/App.js
@@ -3,15 +3,30 @@ import {Route, Switch} from "react-router-dom";
 import Artists from "./containers /Artists /Artists";
 import Albums from "./containers /Albums /Albums";
 import Tracks from "./containers /Tracks/Tracks";
+import {Container} from "reactstrap";
+import Toolbar from "./components /UI/Toolbar/Toolbar";
+import Register from "./containers /Register/Register";
+import Login from "./containers /Login/Login";
+import TrackHistory from "./containers /History /TrackHistory";
 
 const App = () => {
     return (
-        <Switch>
-            <Route path='/' exact component={Artists}/>
-            <Route path='/albums/:id' exact component={Albums}/>
-            <Route path='/albums/tracks/:id' exact component={Tracks}/>
-            <Route render={()=> <h1>Not Found</h1>}/>
-        </Switch>
+        <>
+            <header>
+                <Toolbar/>
+            </header>
+            <Container className='mt-5'>
+                <Switch>
+                    <Route path='/' exact component={Artists}/>
+                    <Route path='/albums/:id' exact component={Albums}/>
+                    <Route path='/albums/tracks/:id' exact component={Tracks}/>
+                    <Route path='/register' exact component={Register}/>
+                    <Route path='/login' exact component={Login}/>
+                    <Route path='/history' exact component={TrackHistory}/>
+                    <Route render={()=> <h1>Not Found</h1>}/>
+                </Switch>
+            </Container>
+        </>
     );
 };
 
