fb2d67a9ae650d6e9cdf0ece3ebf8e56772b30b2
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-03-27 15:40:35 +0600

    HW#89 - отрефакторил

--- music-frontend/src/index.js
+++ music-frontend/src/index.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import {Provider} from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import {createBrowserHistory} from "history";
-import {ConnectedRouter, connectRouter, routerMiddleware} from "connected-react-router";
+import {ConnectedRouter} from "connected-react-router";
 import * as serviceWorker from './serviceWorker';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import store, {history} from "./store /configureStore";
 import App from './App';
-import artistsReducer from "./store /reducers /artistsReducer";
-import albumsReducer from "./store /reducers /albumsReducer";
-import tracksReducer from "./store /reducers /tracksReducer";
-import usersReducer from "./store /reducers /usersReducer";
 
-const history = createBrowserHistory();
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({
-    router: connectRouter(history),
-    artists: artistsReducer,
-    albums: albumsReducer,
-    tracks: tracksReducer,
-    users: usersReducer,
-});
-const middleware = [
-    thunkMiddleware,
-    routerMiddleware(history)
-];
-const enhancers = composeEnhancers(applyMiddleware(...middleware));
-const store = createStore(rootReducer, enhancers);
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = (
     <Provider store={store}>
---
+++ music-frontend/src/store /configureStore.js
@@ -0,0 +1,42 @@
+import {createBrowserHistory} from "history";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import {connectRouter, routerMiddleware} from "connected-react-router";
+
+import thunkMiddleware from "redux-thunk";
+import {loadFromLocalStorage, localStorageMiddleware, saveToLocalStorage} from "./localStorage";
+import usersReducer from "./reducers /usersReducer";
+import artistsReducer from "./reducers /artistsReducer";
+import albumsReducer from "./reducers /albumsReducer";
+import tracksReducer from "./reducers /tracksReducer";
+
+export const history = createBrowserHistory();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+    router: connectRouter(history),
+    artists: artistsReducer,
+    albums: albumsReducer,
+    tracks: tracksReducer,
+    users: usersReducer,
+});
+const middleware = [
+    thunkMiddleware,
+    routerMiddleware(history),
+    localStorageMiddleware
+];
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(rootReducer, persistedState,enhancers);
+
+store.subscribe(() => {
+    saveToLocalStorage({
+        users: {
+            user: store.getState().users.user
+        }
+    })
+});
+
+export default store;
