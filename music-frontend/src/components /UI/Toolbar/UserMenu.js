106f12a02ac4570a077170be6815bd2883d72754
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-03-27 15:41:19 +0600

    HW#89 - добавил выход пользователя

--- music-frontend/src/components /UI/Toolbar/UserMenu.js
+++ music-frontend/src/components /UI/Toolbar/UserMenu.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import {DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink, UncontrolledDropdown} from "reactstrap";
 import {NavLink as RouterNavLink} from "react-router-dom";
-const UserMenu = ({user}) => {
+const UserMenu = ({user, logout}) => {
     return (
         <UncontrolledDropdown nav inNavbar>
             <Nav className="ml-auto" navbar>
                 <NavItem>
                     <NavLink tag={RouterNavLink} to="/history" exact>История прослушивания</NavLink>
                 </NavItem>
+                <NavItem>
+                    <NavLink tag={RouterNavLink} to="/addArtist" exact>Добавить исполнителя</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={RouterNavLink} to="/addAlbum" exact>Добавить альбом</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink tag={RouterNavLink} to="/addTrack" exact>Добавить композицию</NavLink>
+                </NavItem>
+                {user.role === 'admin' ? (<NavItem>
+                    <NavLink tag={RouterNavLink} to="/admin_office" exact>Офис админа</NavLink>
+                </NavItem>): null}
             </Nav>
             <DropdownToggle nav caret>
                 Hello, {user.username}!
@@ -17,7 +29,7 @@ const UserMenu = ({user}) => {
                     View profile
                 </DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem>
+                <DropdownItem onClick={logout}>
                     Logout
                 </DropdownItem>
             </DropdownMenu>
