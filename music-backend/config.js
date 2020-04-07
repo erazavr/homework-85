b1a5472d23f3505b27f78ffa8d9ae0fa1977a1d5
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-04-07 11:20:22 +0600

    HW#92 - добавил поле facebook

--- music-backend/config.js
+++ music-backend/config.js
@@ -10,5 +10,9 @@ module.exports = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
+  },
+  facebook: {
+    appId: '153072742695088',
+    appSecret: '08a0982a23ce6d06f576ba5b04526299'
   }
 };
